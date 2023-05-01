31dad33e12064c40fb0790e6d1da2c60fb9216d0
Abhishek <abhisheksinghasr734@gmail.com>
2023-05-01 09:06:18 +0530

    js practice

---
+++ index.js
@@ -0,0 +1,36 @@
+// console.log(5===4);
+
+// let test1 = true && "banana";
+
+// console.log(test1);
+
+
+// let test2 = undefined ?? "banana2";
+// // let test2 = null ?? "banana2";
+// console.log((3>2)===true);
+
+// console.log("---Animal Checker Program---");
+
+// let animal = "Dog";
+// if(animal === "Cat"){
+//     console.log("Meow");
+// }else if(animal==="Dog"){
+//     console.log("Woof");
+// }else{
+//     console.log("Must be an alien");
+// }
+
+// console.log("---Animal Check Complete")
+
+let chill = ()=>{
+    console.log("Doing some chilling");
+    for(let i =1;i<=10;i++){
+        console.log(`Chill...${i}`);
+    }
+    console.log("That was ice cold");
+}
+chill();
+
+
+
+
